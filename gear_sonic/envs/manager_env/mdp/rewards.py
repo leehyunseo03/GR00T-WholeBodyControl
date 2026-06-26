fix: fix quaternion Issue

gear_sonic/envs/manager_env/mdp/rewards.py
@@ -6,19 +6,18 @@ from typing import TYPE_CHECKING
 
 from isaaclab.managers import SceneEntityCfg
 from isaaclab.utils import configclass
-from isaaclab.utils.math import (
-    quat_apply,
-    quat_error_magnitude,
-    quat_inv,
-    quat_mul,
-)
 import torch
 
 from gear_sonic.envs.manager_env.mdp.commands import (
     ForceTrackingCommand,
     TrackingCommand,
     _get_body_indexes,
+    quat_apply,
+    quat_error_magnitude,
+    quat_inv,
+    quat_mul,
 )
+from gear_sonic.isaac_utils import quaternion_adapter as quat_adapter
 
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
@@ -128,7 +127,7 @@ def upright_penalty(
     total_penalty = torch.zeros(env.num_envs, device=env.device)
     for name in body_names:
         body_idx = robot.body_names.index(name)
-        body_quat = robot.data.body_quat_w[:, body_idx]
+        body_quat = quat_adapter.isaaclab_to_wxyz(robot.data.body_quat_w[:, body_idx])
         g_local = quat_apply(quat_inv(body_quat), command.down_dir)
         total_penalty += g_local[:, 0] ** 2 + g_local[:, 1] ** 2
 

gear_sonic/trl/callbacks/im_eval_callback.py
@@ -10,6 +10,9 @@ from tqdm import tqdm
 from transformers import TrainerCallback
 import wandb
 
+from gear_sonic.envs.manager_env.mdp.commands import quat_error_magnitude
+from gear_sonic.isaac_utils import quaternion_adapter as quat_adapter
+
 
 def create_html_table(metrics_dict):
     """
@@ -307,15 +310,13 @@ class ImEvalCallback(TrainerCallback):
             obj = self.env.env.scene["object"]
             motion_cmd = self.env.motion_command
             current_obj_pos = obj.data.root_pos_w[:, :3]  # (num_envs, 3)
-            current_obj_quat = obj.data.root_quat_w  # (num_envs, 4)
+            current_obj_quat = quat_adapter.isaaclab_to_wxyz(obj.data.root_quat_w)
             target_obj_pos = motion_cmd.object_root_pos[:, 0, :3]  # (num_envs, 3)
             target_obj_quat = motion_cmd.object_root_quat[:, 0]  # (num_envs, 4)
 
             pos_error = torch.norm(target_obj_pos - current_obj_pos, dim=-1)  # (num_envs,)
 
             # Quaternion error: angle between two quaternions
-            from isaaclab.utils.math import quat_error_magnitude
-
             ori_error = quat_error_magnitude(target_obj_quat, current_obj_quat)  # (num_envs,)
 
             self.obj_pos_error.append(pos_error.cpu())